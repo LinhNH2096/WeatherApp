fb3fa53693eeea8a35de68626370bd9c3f7f3d3b
LinhNH2096 <linh.nh@neo-lab.vn>
2024-02-22 10:40:19 +0700

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by LinhNguyen DGWA on 22/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
